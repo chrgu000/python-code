8f50f6f23399ffd3207fe662121c325e47f46edc
WWWWBing <huanghe9392@sina.com>
2018-01-17 11:21:38 +0800

    sublimtText3 PEP8格式化插件测试
    Python PEP8 Autoformat
    Python Flak8 lint

--- dict.py
+++ dict.py
@@ -1,31 +1,32 @@
-#!/use/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-#输入姓名查询成绩
+# 输入姓名查询成绩
 
-#录入姓名和成绩
+# 录入姓名和成绩
+import time
+
 d = {
-'小明' : 99,
-'小刚' : 83,
-'小芳' : 100,
-'小王' : 85,
-'小李' : 74,
-'小张' : 42,
-'小飞' : 19
+    '小明': 99,
+    '小刚': 83,
+    '小芳': 100,
+    '小王': 85,
+    '小李': 74,
+    '小张': 42,
+    '小飞': 19
 }
 
-#显示所有录入姓名
+# 显示所有录入姓名
 print('本次参加考试的人员有：\n')
 for k in d:
     print(k, end=" ")
-    
-#用户输入姓名
+
+# 用户输入姓名
 name = input('\n\n请输入查询姓名：\t')
 
 if name in d:
     print('\n\n', name, '的成绩是：', d[name])
 else:
     print('\n\n查无此人')
-    
-import time
-print(time.strftime('%Y-%m-%d %H:%M:%S'), '星期',  time.strftime('%w')) 
+
+
+print(time.strftime('%Y-%m-%d %H:%M:%S'), '星期', time.strftime('%w'))
