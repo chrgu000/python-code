5aabe8c2a39d440788b64d1636f3fb973a6f214f
Administrator <wsex321@yahoo.com>
2019-08-19 16:47:52 +0800

    java环境添加到用户目录 下

--- mochain/script/install_jdk.sh
+++ mochain/script/install_jdk.sh
@@ -10,18 +10,18 @@ echo "Now start download jdk-8u212-linux-x64.tar.gz"
 wget https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-x64.tar.gz
 tar zxvf jdk-8u212-linux-x64.tar.gz -C /usr/local/
 
-if  ! ``grep -n "JAVA_HOME=" /etc/profile > /dev/null``; then
+if  ! ``grep -n "JAVA_HOME=" ~/.bashrc > /dev/null``; then
 		echo "add jdk path to system"
-	    echo '' >> /etc/profile
-	    echo '# java ' >> /etc/profile
-	    echo 'export JAVA_HOME=/usr/local/jdk1.8.0_212' >> /etc/profile
-	    echo 'export JRE_HOME=${JAVA_HOME}/jre' >> /etc/profile
-	    echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> /etc/profile
-	    echo 'export  PATH=${JAVA_HOME}/bin:$PATH' >> /etc/profile
+	    echo '' >> ~/.bashrc
+	    echo '# java ' >> ~/.bashrc
+	    echo 'export JAVA_HOME=/usr/local/jdk1.8.0_212' >> ~/.bashrc
+	    echo 'export JRE_HOME=${JAVA_HOME}/jre' >> ~/.bashrc
+	    echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> ~/.bashrc
+	    echo 'export  PATH=${JAVA_HOME}/bin:$PATH' >> ~/.bashrc
         sleep 5
-	    source /etc/profile
+	    source ~/.bashrc
 fi
 # delete source file
 rm -rf jdk-8u212-linux-x64.tar.gz
 
-source /etc/profile
+source ~/.bashrc
